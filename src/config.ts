9322f2c4a699a1702af96e750a1882090c899d16
CorrectRoadH <a778917369@gmail.com>
2025-07-29 11:14:35 +0800

    add apple icon

--- src/config.ts
+++ src/config.ts
@@ -8,67 +8,72 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: `CorrectRoad's Blog`,
-  subtitle: '时来天地皆同力，运去英雄不自由。',
-  lang: 'zh_CN', // 'en', 'zh_CN', 'zh_TW', 'ja'
-  themeColor: {
-    hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false, // Hide the theme color picker for visitors
-  },
-  banner: {
-    enable: false,
-    src: 'assets/images/demo-banner.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center', // Equivalent to object-position, defaults center
-    credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
-    }
-  },
-  favicon: [
-    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
-  ],
-  toc: {
-    enable: false,
-    depth: 1,
-  },
-}
+	title: `CorrectRoad's Blog`,
+	subtitle: "时来天地皆同力，运去英雄不自由。",
+	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja'
+	themeColor: {
+		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: false, // Hide the theme color picker for visitors
+	},
+	banner: {
+		enable: false,
+		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		position: "center", // Equivalent to object-position, defaults center
+		credit: {
+			enable: false, // Display the credit text of the banner image
+			text: "", // Credit text to be displayed
+			url: "", // (Optional) URL link to the original artwork or artist's page
+		},
+	},
+	favicon: [
+		// Leave this array empty to use the default favicon
+		// {
+		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		// }
+	],
+	toc: {
+		enable: false,
+		depth: 1,
+	},
+};
 
 export const navBarConfig: NavBarConfig = {
-  links: [
-    LinkPreset.Home,
-    LinkPreset.Archive,
-    LinkPreset.About,
-    {
-      name: 'GitHub',
-      url: 'https://github.com/CorrectRoadH', // Internal links should not include the base path, as it is automatically added
-      external: true, // Show an external link icon and will open in a new tab
-    },
-  ],
-}
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		{
+			name: "GitHub",
+			url: "https://github.com/CorrectRoadH", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
+		},
+	],
+};
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',
-  name: 'CorrectRoad',
-  bio: '时来天地皆同力，运去英雄不自由。',
-  links: [
-    {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',
-      url: 'https://twitter.com/CorrectRoad',
-    },
-    {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/CorrectRoadH',
-    },
-  ],
-}
+	avatar: "assets/images/demo-avatar.png",
+	name: "CorrectRoad",
+	bio: "时来天地皆同力，运去英雄不自由。",
+	links: [
+		{
+			name: "Twitter",
+			icon: "fa6-brands:twitter",
+			url: "https://twitter.com/CorrectRoad",
+		},
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/CorrectRoadH",
+		},
+		{
+			name: "Flash ISO",
+			icon: "fa6-brands:apple",
+			url: "https://www.l12t.com/flashiso/",
+		},
+	],
+};
 
 export const licenseConfig: LicenseConfig = {
 	enable: true,
