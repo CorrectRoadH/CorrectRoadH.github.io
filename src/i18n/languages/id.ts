0214fb2c4e128ea8ea7a36f257bf046746c7bb53
CorrectRoadH <a778917369@gmail.com>
2026-01-13 23:36:32 +0800

    fix

--- src/i18n/languages/id.ts
+++ src/i18n/languages/id.ts
@@ -6,6 +6,7 @@ export const id: Translation = {
 	[Key.about]: "Tentang",
 	[Key.archive]: "Arsip",
 	[Key.search]: "Cari",
+	[Key.friends]: "Teman",
 
 	[Key.tags]: "Tag",
 	[Key.categories]: "Kategori",
--- src/i18n/languages/ja.ts
+++ src/i18n/languages/ja.ts
@@ -6,6 +6,7 @@ export const ja: Translation = {
 	[Key.about]: "About",
 	[Key.archive]: "Archive",
 	[Key.search]: "検索",
+	[Key.friends]: "友達",
 
 	[Key.tags]: "タグ",
 	[Key.categories]: "カテゴリ",
